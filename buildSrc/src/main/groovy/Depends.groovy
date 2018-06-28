c2dbf39ee521f6762e6867468ff4ff4e4fecc4a5
Satoru Sukawa <sukawasatoru.github@outlook.jp>
2018-06-28 23:08:23 +0900

    Bump up the Android Plugin version to 3.1.3

--- buildSrc/src/main/groovy/Depends.groovy
+++ buildSrc/src/main/groovy/Depends.groovy
@@ -1,64 +1,64 @@
 class Depends {
     static final arch = new Arch()
-    static final errorprone = "com.google.errorprone:error_prone_core:$Version.errorprone"
+    static final errorprone = "com.google.errorprone:error_prone_core:$Versions.errorprone"
     static final dagger = new Dagger()
     static final glide = new Glide()
     static final gradle = new Gradle()
-    static final kotlin = "org.jetbrains.kotlin:kotlin-stdlib:$Version.kotlin"
+    static final kotlin = "org.jetbrains.kotlin:kotlin-stdlib:$Versions.kotlin"
     static final leakcanary = new Leakcanary()
     static final orma = new Orma()
     static final rx = new Rx()
     static final support = new Support()
 
     static class Arch {
-        final lifecycle = "android.arch.lifecycle:extensions:$Version.arch.live"
-        final lifecycleCompiler = "android.arch.lifecycle:compiler:$Version.arch.live"
+        final lifecycle = "android.arch.lifecycle:extensions:$Versions.arch.live"
+        final lifecycleCompiler = "android.arch.lifecycle:compiler:$Versions.arch.live"
     }
 
     static class Dagger {
-        final dagger = "com.google.dagger:dagger:$Version.dagger"
-        final compiler = "com.google.dagger:dagger-compiler:$Version.dagger"
+        final dagger = "com.google.dagger:dagger:$Versions.dagger"
+        final compiler = "com.google.dagger:dagger-compiler:$Versions.dagger"
     }
 
     static class Glide {
-        final glide = "com.github.bumptech.glide:glide:$Version.glide"
-        final okhttp3 = "com.github.bumptech.glide:okhttp3-integration:$Version.glide"
+        final glide = "com.github.bumptech.glide:glide:$Versions.glide"
+        final okhttp3 = "com.github.bumptech.glide:okhttp3-integration:$Versions.glide"
     }
 
     static class Gradle {
-        final android = "com.android.tools.build:gradle:$Version.gradle.android"
+        final android = "com.android.tools.build:gradle:$Versions.gradle.android"
         final androidCacheFix =
-                "gradle.plugin.org.gradle.android:android-cache-fix-gradle-plugin:$Version.gradle.androidCacheFix"
-        final kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$Version.kotlin"
-        final errorprone = "net.ltgt.gradle:gradle-errorprone-plugin:$Version.gradle.errorprone"
+                "gradle.plugin.org.gradle.android:android-cache-fix-gradle-plugin:$Versions.gradle.androidCacheFix"
+        final kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$Versions.kotlin"
+        final errorprone = "net.ltgt.gradle:gradle-errorprone-plugin:$Versions.gradle.errorprone"
     }
 
     static class Leakcanary {
-        final debug = "com.squareup.leakcanary:leakcanary-android:$Version.leakcanary"
-        final release = "com.squareup.leakcanary:leakcanary-android-no-op:$Version.leakcanary"
-        final test = "com.squareup.leakcanary:leakcanary-android-no-op:$Version.leakcanary"
+        final debug = "com.squareup.leakcanary:leakcanary-android:$Versions.leakcanary"
+        final release = "com.squareup.leakcanary:leakcanary-android-no-op:$Versions.leakcanary"
+        final test = "com.squareup.leakcanary:leakcanary-android-no-op:$Versions.leakcanary"
     }
 
     static class Orma {
-        final orma = "com.github.gfx.android.orma:orma:$Version.orma"
-        final compiler = "com.github.gfx.android.orma:orma-processor:$Version.orma"
+        final orma = "com.github.gfx.android.orma:orma:$Versions.orma"
+        final compiler = "com.github.gfx.android.orma:orma-processor:$Versions.orma"
     }
 
     static class Rx {
-        final java = "io.reactivex.rxjava2:rxjava:$Version.rx.java"
-        final android = "io.reactivex.rxjava2:rxandroid:$Version.rx.android"
+        final java = "io.reactivex.rxjava2:rxjava:$Versions.rx.java"
+        final android = "io.reactivex.rxjava2:rxandroid:$Versions.rx.android"
     }
 
     static class Support {
-        final constraint = "com.android.support.constraint:constraint-layout:$Version.support.constraint"
-        final annotations = "com.android.support:support-annotations:$Version.support.support"
-        final recyclerview = "com.android.support:recyclerview-v7:$Version.support.support"
-        final cardview = "com.android.support:cardview-v7:$Version.support.support"
-        final leanback = "com.android.support:leanback-v17:$Version.support.support"
-        final tvProvider = "com.android.support:support-tv-provider:$Version.support.support"
-        final compat = "com.android.support:support-compat:$Version.support.support"
-        final compatV4 = "com.android.support:support-v4:$Version.support.support"
-        final compatV7 = "com.android.support:appcompat-v7:$Version.support.support"
-        final coreUtils = "com.android.support:support-core-utils:$Version.support.support"
+        final constraint = "com.android.support.constraint:constraint-layout:$Versions.support.constraint"
+        final annotations = "com.android.support:support-annotations:$Versions.support.support"
+        final recyclerview = "com.android.support:recyclerview-v7:$Versions.support.support"
+        final cardview = "com.android.support:cardview-v7:$Versions.support.support"
+        final leanback = "com.android.support:leanback-v17:$Versions.support.support"
+        final tvProvider = "com.android.support:support-tv-provider:$Versions.support.support"
+        final compat = "com.android.support:support-compat:$Versions.support.support"
+        final compatV4 = "com.android.support:support-v4:$Versions.support.support"
+        final compatV7 = "com.android.support:appcompat-v7:$Versions.support.support"
+        final coreUtils = "com.android.support:support-core-utils:$Versions.support.support"
     }
 }
--- buildSrc/src/main/groovy/Versions.groovy
+++ buildSrc/src/main/groovy/Versions.groovy
@@ -1,10 +1,10 @@
-class Version {
+class Versions {
     static final arch = new Arch()
-    static final errorprone = '2.2.0'
-    static final dagger = '2.14.1'
+    static final errorprone = '2.3.1'
+    static final dagger = '2.16'
     static final gradle = new Gradle()
-    static final glide = '4.6.1'
-    static final kotlin = '1.2.20'
+    static final glide = '4.7.1'
+    static final kotlin = '1.2.50'
     static final leakcanary = '1.5.4'
     static final manifest = new Manifest()
     static final orma = '4.2.5'
@@ -16,25 +16,25 @@ class Version {
     }
 
     static class Gradle {
-        final android = '3.1.0'
-        final androidCacheFix = '0.3.6'
-        final errorprone = '0.0.13'
+        final android = '3.1.3'
+        final androidCacheFix = '0.5.1'
+        final errorprone = '0.0.14'
     }
 
     static class Manifest {
-        final compileSdk = 27
-        final buildTools = '27.0.3'
+        final compileSdk = 28
+        final buildTools = '28.0.0'
         final minSdk = 21
-        final targetSdk = 27
+        final targetSdk = 28
     }
 
     static class Rx {
-        final java = '2.1.12'
+        final java = '2.1.16'
         final android = '2.0.2'
     }
 
     static class Support {
-        final support = '27.1.0'
-        final constraint = '1.0.2'
+        final support = '27.1.1'
+        final constraint = '1.1.2'
     }
 }
