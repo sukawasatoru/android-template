ff6d74d21939fce9ceb52ee399eeabe42aec46b5
Satoru Sukawa <sukawasatoru.github@outlook.jp>
2020-12-02 01:09:18 +0900

    Update libraries

--- buildSrc/src/main/groovy/Depends.groovy
+++ buildSrc/src/main/groovy/Depends.groovy
@@ -9,17 +9,12 @@ class Depends {
     static class AndroidX {
         final activity = 'androidx.activity:activity-ktx:1.1.0'
         final annotation = 'androidx.annotation:annotation:1.1.0'
-        final constraint = 'androidx.constraintlayout:constraintlayout:2.0.0'
-        final datastore = 'androidx.datastore:datastore-core:1.0.0-alpha01'
+        final constraint = 'androidx.constraintlayout:constraintlayout:2.0.4'
+        final datastore = 'androidx.datastore:datastore-core:1.0.0-alpha04'
         final fragment = 'androidx.fragment:fragment-ktx:1.2.5'
         final leanback = 'androidx.leanback:leanback:1.0.0'
-        final lifecycleVersion = '2.2.0'
-        final lifecycleCompiler = "androidx.lifecycle:lifecycle-compiler:$lifecycleVersion"
-        final lifecycleLiveData = "androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion"
-        final lifecycleRuntime = "androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion"
-        final lifecycleViewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion"
-        final lifecycleViewModelSavedState = "androidx.lifecycle:lifecycle-viewmodel-savedState:$lifecycleVersion"
-        final navigationVersion = '2.3.0'
+        final lifecycleRuntime = 'androidx.lifecycle:lifecycle-runtime-ktx:2.2.0'
+        final navigationVersion = '2.3.1'
         final navigationFragment = "androidx.navigation:navigation-fragment-ktx:$navigationVersion"
         final navigationUi = "androidx.navigation:navigation-ui-ktx:$navigationVersion"
         final roomVersion = '2.2.5'
@@ -29,7 +24,8 @@ class Depends {
     }
 
     static class Google {
-        final hiltVersion = '2.29-alpha'
+        final androidOssLicenses = 'com.google.android.gms:play-services-oss-licenses:17.0.0'
+        final hiltVersion = '2.30.1-alpha'
         final hilt = "com.google.dagger:hilt-android:$hiltVersion"
         final hiltCompiler = "com.google.dagger:hilt-android-compiler:$hiltVersion"
         final material = 'com.google.android.material:material:1.2.1'
@@ -39,21 +35,23 @@ class Depends {
     }
 
     static class Kotlin {
-        final coroutinesVersion = '1.3.9'
+        final coroutinesVersion = '1.4.2'
         final coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion"
         final coroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion"
-        final stdlib = 'org.jetbrains.kotlin:kotlin-stdlib:1.4.10'
-        final serialization = 'org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.20.0'
+        final stdlib = 'org.jetbrains.kotlin:kotlin-stdlib:1.4.20'
+        final serialization = 'org.jetbrains.kotlinx:kotlinx-serialization-runtime:1.0.1'
     }
 
     static class Test {
         final androidJunit4Ktx = 'androidx.test.ext:junit-ktx:1.1.2'
         final junit = 'junit:junit:4.12'
-        final robolectric = 'org.robolectric:robolectric:4.3'
-        final testCoreKtx = 'androidx.test:core-ktx:1.3.0'
-        final testRunner = 'androidx.test:runner:1.3.0'
-        final testRules = 'androidx.test:rules:1.3.0'
-        final truth = 'com.google.truth:truth:1.0.1'
-        final truthJava8 = 'com.google.truth.extensions:truth-java8-extension:1.0.1'
+        final robolectric = 'org.robolectric:robolectric:4.4'
+        final testVersion = '1.3.0'
+        final testCoreKtx = "androidx.test:core-ktx:$testVersion"
+        final testRunner = "androidx.test:runner:$testVersion"
+        final testRules = "androidx.test:rules:$testVersion"
+        final truthVersion = '1.1'
+        final truth = "com.google.truth:truth:$truthVersion"
+        final truthJava8 = "com.google.truth.extensions:truth-java8-extension:$truthVersion"
     }
 }
