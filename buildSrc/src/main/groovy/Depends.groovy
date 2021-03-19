2d3c04c273784770334e51e78b0a2573b98036ea
Satoru Sukawa <sukawasatoru.github@outlook.jp>
2021-03-20 01:16:16 +0900

    Update libraries

--- buildSrc/src/main/groovy/Depends.groovy
+++ buildSrc/src/main/groovy/Depends.groovy
@@ -1,21 +1,28 @@
 class Depends {
     static final androidx = new AndroidX()
+    static final apollo = new Apollo()
     static final google = new Google()
     static final kotlin = new Kotlin()
     static final kotlinResult = 'com.michael-bull.kotlin-result:kotlin-result:1.1.9'
     static final okhttp = 'com.squareup.okhttp3:okhttp:4.9.0'
     static final test = new Test()
+    static final tinyport = new Tinyport()
 
     static class AndroidX {
         final activity = 'androidx.activity:activity-ktx:1.2.1'
         final annotation = 'androidx.annotation:annotation:1.1.0'
+        final appcompat = 'androidx.appcompat:appcompat:1.2.0'
         final constraint = 'androidx.constraintlayout:constraintlayout:2.0.4'
         final datastore = 'androidx.datastore:datastore-core:1.0.0-alpha08'
         final fragment = 'androidx.fragment:fragment-ktx:1.3.1'
         final leanback = 'androidx.leanback:leanback:1.0.0'
         final lifecycleVersion = '2.3.0'
+        final lifecycleCompiler = "androidx.lifecycle:lifecycle-compiler:$lifecycleVersion"
         final lifecycleLiveData = "androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion"
         final lifecycleRuntime = "androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion"
+        final lifecycleService = "androidx.lifecycle:lifecycle-service:$lifecycleVersion"
+        final lifecycleViewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion"
+        final lifecycleViewModelSavedState = "androidx.lifecycle:lifecycle-viewmodel-savedstate:$lifecycleVersion"
         final navigationVersion = '2.3.4'
         final navigationFragment = "androidx.navigation:navigation-fragment-ktx:$navigationVersion"
         final navigationUi = "androidx.navigation:navigation-ui-ktx:$navigationVersion"
@@ -24,6 +31,15 @@ class Depends {
         final roomCompiler = "androidx.room:room-compiler:$roomVersion"
         final roomKtx = "androidx.room:room-ktx:$roomVersion"
         final roomRuntime = "androidx.room:room-runtime:$roomVersion"
+    }
+
+    static class Apollo {
+        final version = '2.5.4'
+        final androidSupport = "com.apollographql.apollo:apollo-android-support:$version"
+        final api = "com.apollographql.apollo:apollo-api:$version"
+        final cacheSqlite = "com.apollographql.apollo:apollo-normalized-cache-sqlite:$version"
+        final coroutine = "com.apollographql.apollo:apollo-coroutines-support:$version"
+        final runtime = "com.apollographql.apollo:apollo-runtime:$version"
     }
 
     static class Google {
@@ -41,8 +57,8 @@ class Depends {
         final coroutinesVersion = '1.4.2'
         final coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion"
         final coroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion"
-        final stdlib = 'org.jetbrains.kotlin:kotlin-stdlib:1.4.31'
         final serializationJson = 'org.jetbrains.kotlinx:kotlinx-serialization-json:1.1.0'
+        final stdlib = 'org.jetbrains.kotlin:kotlin-stdlib:1.4.31'
     }
 
     static class Test {
@@ -56,5 +72,9 @@ class Depends {
         final truthVersion = '1.1'
         final truth = "com.google.truth:truth:$truthVersion"
         final truthJava8 = "com.google.truth.extensions:truth-java8-extension:$truthVersion"
+    }
+
+    static class Tinyport {
+        final logger = 'jp.tinyport:logger:0.1.0'
     }
 }
